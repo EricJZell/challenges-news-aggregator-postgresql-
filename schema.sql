c77d862911ba0c903abd9aa391d4ca42c1abd5a7
Eric Zell <EricJZell@gmail.com>
2015-10-25 12:54:10 -0400

    seed database a little

--- schema.sql
+++ schema.sql
@@ -1,9 +1,4 @@
--- If you want to run this schema repeatedly you'll need to drop
--- the table before re-creating it. Note that you'll lose any
--- data if you drop and add a table:
-
 DROP TABLE IF EXISTS articles;
-
 
 CREATE TABLE articles (title varchar(255), url varchar(255), description varchar(1023));
 CREATE UNIQUE INDEX url ON articles (url);
